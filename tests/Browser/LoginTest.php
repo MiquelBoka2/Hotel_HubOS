23b81447f25ea9587b531dc77748bd4a26ffabd9
miquelweb <miquelweb@gmail.com>
2022-07-22 08:01:01 +0200

    Initial installation

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', 'admin@mail.com')
+                ->type('password', '123456')
+                ->press('Login')
+                ->assertSee('Dashboard');
+        });
+    }
+}
